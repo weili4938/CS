69a0953898cd9e68809add4b3a50bc177e719691
Wei Li <w.li@salesforce.com>
2020-07-03 18:17:13 -0700

    first commit

---
+++ src/Starting_Template.java
@@ -0,0 +1,24 @@
+import becker.robots.*;
+/*
+ * Starting Template:
+ * This file was created in order to provide you with a pre made 'starter' program
+ */
+
+public class Starting_Template extends java.lang.Object 
+{
+	public static void main(String[] args) 
+   {
+		City toronto = new City();
+		Robot jo = new Robot(toronto, 3, 0, Direction.EAST, 0);
+		new Thing(toronto, 3, 2);
+
+		/* Your code should go below this comment: */
+		jo.move();
+		jo.move();
+		jo.turnLeft();
+		jo.turnLeft();
+		jo.turnLeft();
+		jo.move();
+		jo.move();
+	} 
+}
